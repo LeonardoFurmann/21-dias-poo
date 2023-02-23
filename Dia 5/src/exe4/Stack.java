cfb4470b6c52bc65d2ceff56ff5221a211ada16c
Leonardo Furmann <leohitnerf@gmail.com>
2023-02-23 10:21:39 -0300

    Create Stack.java

---
+++ Dia 5/src/exe4/Stack.java
@@ -0,0 +1,25 @@
+package exe4;
+
+import java.util.Vector;
+
+public class Stack {
+	
+	Vector vector; // aray de objetos
+	
+	public boolean empty() {
+		return false;
+	}
+	public Object peek() {
+		return null;
+	}
+	public Object pop() {
+		return null;
+	}
+	public Object push(Object item) {
+		return null;
+	}
+	public int search(Object o ) {
+		return 0;
+	}
+
+}
