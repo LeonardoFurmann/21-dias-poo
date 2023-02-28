5dbed1bd753b1907528428aaafd5184c7dd8f3f9
Leonardo Furmann <leohitnerf@gmail.com>
2023-02-28 17:39:50 -0300

    entendendo interface

---
+++ Interface/src/Stack.java
@@ -0,0 +1,11 @@
+
+public interface Stack {
+	
+		public boolean empty(); 
+		public Object peek(); 
+		public Object pop (); 
+		public Object push( Object item ) ; 
+		public int search( Object o ) ; 
+	
+
+}
