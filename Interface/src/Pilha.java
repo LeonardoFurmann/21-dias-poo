08b44bc2e5a546d448576c096899ea4f8e441bbf
Leonardo Furmann <leohitnerf@gmail.com>
2023-03-03 17:33:35 -0300

    Update Pilha.java

--- Interface/src/Pilha.java
+++ Interface/src/Pilha.java
@@ -1,4 +1,34 @@
 
-public class Pilha {
+public class Pilha implements Stack{
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object push(Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int search(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
