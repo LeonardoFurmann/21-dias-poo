aa37861ab3db556455cf28b5fe3690e7c5869097
Leonardo Furmann <leohitnerf@gmail.com>
2023-03-03 17:33:44 -0300

    dia 6 conversão

---
+++ Dia6/src/Conversão.java
@@ -0,0 +1,23 @@
+
+public class Conversão {
+
+	public static void main(String[] args) {
+		
+		
+		//add(1000000000, 2);
+		
+		//adicionar(a, b);
+
+	}
+	
+//	public static void add(double a, double b) {
+//		
+//		System.out.println(Math.max(a, b));
+//	}
+//	
+//	public static void adicionar([T] a ,[T] String b) {
+//		
+//		System.out.println(a + b);
+//	}
+
+}
